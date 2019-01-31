b343bb55be96f9bfc5b22bd73ba90a95d33a8a9d
souj <soujanya.venkatesh@gmail.com>
2019-01-31 18:43:45 +0530

    Reorganized dependencies in package.json

--- Documents/Cloned-Repo/enlyter/app.js
+++ Documents/Cloned-Repo/enlyter/app.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(path.join(__dirname, '/enlyter/dist')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('*', (req,res) => {
-  res.sendFile(path.join(__dirname, '/enlyter/dist/enlyter/index.html'));
+  res.sendFile(path.join(__dirname, 'dist/enlyter/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
