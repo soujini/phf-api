d047c94194ff2bc4c9aa737a188fb24a677ddee2
souj <soujanya.venkatesh@gmail.com>
2020-06-30 17:06:03 +0530

    Added Env

--- app.js
+++ app.js
@@ -4,14 +4,6 @@ var bodyParser = require('body-parser');
 var app=express();
 const path = require('path');
 const dotenv = require('dotenv').config({ path: path.join(__dirname, '../.env') });
-
-// const dotenv = require('dotenv')
-// dotenv.config({
-//   path: path.join(__dirname, '../.env'),
-// })
-
-// require('dotenv').config({ path: path.join(__dirname, '.env') });
-
 const hostname = '127.0.0.1';
 const port = process.env.PORT || 3000;
 
@@ -35,10 +27,9 @@ var smtpTransport = nodemailer.createTransport({
 });
 
 app.post('/send',function(req,res){
-  console.log("souj "+process.env.USER_EMAIL);
   var mailOptions={
     from: req.body.email,
-    to : "soujanya.venkatesh@gmail.com",
+    to : "hello@prataphealthfoods.com",
     subject : "Enquiry",
     html : "<div><p>Hello,<p>Name: " +req.body.name+"</p><p>Email: "+req.body.email+"</p><p>Subject: "+req.body.subject+"</p><p>Message: "+req.body.message+"</p><p>Thanks,</p><p>Pratap Health & Foods (India) Private Limited</p></div>"
   }
